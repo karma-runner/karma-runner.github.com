cbdd4d1d72960a80467852ccf1674195a5123315
dignifiedquire <dignifiedquire@gmail.com>
2015-06-03 23:13:43 +0200

    Cleanup and add linting
    * Upgrade Jade to 1.10
    * Move to standard JavaScript
    * No more coffeescript gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,98 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    eslint: {
+      target: [
+        'gruntfile.js',
+        'tasks/*.js'
+      ]
+    },
+
+    less: {
+      docs: {
+        files: {
+          'assets/css/app.css': 'src/less/app.less'
+        }
+      }
+    },
+
+    cssmin: {
+      docs: {
+        files: {
+          'assets/css/app.css': 'assets/css/app.css'
+        }
+      }
+    },
+
+    uglify: {
+      docs: {
+        files: {
+          'assets/js/app.js': [
+            'src/js/jquery.js',
+            'src/js/jquery.easing.js',
+            'src/js/ddsmoothmenu.js',
+            'src/js/jquery.flexslider.js',
+            'src/js/colortip.js',
+            'src/js/selectnav.js',
+            'src/js/custom.js'
+          ]
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          base: '.',
+          port: 8000,
+          keepalive: true
+        }
+      }
+    },
+
+    'static': {
+      options: {
+        template: 'src/jade'
+      },
+      docs: {
+        files: {
+          '.': 'src/content'
+        }
+      }
+    },
+
+    watch: {
+      less: {
+        files: 'src/less/**/*.less',
+        tasks: ['less:docs', 'cssmin:docs']
+      },
+      js: {
+        files: 'src/js/*.js',
+        tasks: ['uglify:docs']
+      },
+      jade: {
+        files: ['src/content/**/*.md', 'src/jade/**/*.jade'],
+        tasks: ['static:docs']
+      }
+    }
+  })
+
+  grunt.loadTasks('tasks')
+  require('load-grunt-tasks')(grunt)
+
+  grunt.registerTask('build', [
+    'less:docs',
+    'cssmin:docs',
+    'uglify:docs',
+    'static:docs'
+  ])
+
+  grunt.registerTask('lint', 'eslint')
+  grunt.registerTask('server', 'connect:server')
+
+  grunt.registerTask('default', [
+    'lint',
+    'build',
+    'server',
+    'watch'
+  ])
+}
--- tasks/static.js
+++ tasks/static.js
@@ -4,87 +4,82 @@
  * @author Vojta Jina <vojta.jina@gmail.com>
  */
 
-var path = require('path');
+var path = require('path')
 
-var q = require('q');
-var fs = require('q-io/fs');
-var namp = require('namp');
-var jade = require('jade');
-var semver = require('semver');
+var q = require('q')
+var fs = require('q-io/fs')
+var namp = require('namp')
+var jade = require('jade')
+var semver = require('semver')
 
-
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
   // Helper Methods
 
-  var urlFromFilename = function(fileName) {
-    return fileName.replace(/^\d*-/, '').replace(/md$/, 'html');
-  };
+  var urlFromFilename = function (fileName) {
+    return fileName.replace(/^\d*-/, '').replace(/md$/, 'html')
+  }
 
-  var menuTitleFromFilename = function(fileName) {
-    return fileName.replace(/^\d*-/, '').replace(/\.md$/, '').split(/[\s_-]/).map(function(word) {
-      return word.charAt(0).toUpperCase() + word.substr(1);
-    }).join(' ');
-  };
+  var menuTitleFromFilename = function (fileName) {
+    return fileName.replace(/^\d*-/, '').replace(/\.md$/, '').split(/[\s_-]/).map(function (word) {
+      return word.charAt(0).toUpperCase() + word.substr(1)
+    }).join(' ')
+  }
 
-  var pageTitleFromFilename = menuTitleFromFilename;
+  var pageTitleFromFilename = menuTitleFromFilename
 
-  var editUrlFromFilenameAndSection = function(fileName, section) {
-    return 'https://github.com/karma-runner/karma/edit/master/docs/' + section + '/' + fileName;
-  };
+  var editUrlFromFilenameAndSection = function (fileName, section) {
+    return 'https://github.com/karma-runner/karma/edit/master/docs/' + section + '/' + fileName
+  }
 
-  var filterOnlyFiles = function(path, stat) {
+  var filterOnlyFiles = function (path, stat) {
     // ignore dot files like .DS_Store
-    var filename = path.split('/').pop();
-    return stat.isFile() && !filename.match(/^\./);
-  };
+    var filename = path.split('/').pop()
+    return stat.isFile() && !filename.match(/^\./)
+  }
 
-  var sortByVersion = function(a, b) {
-    return semver.lt(a + '.0', b + '.0');
-  };
-
+  var sortByVersion = function (a, b) {
+    return semver.lt(a + '.0', b + '.0')
+  }
 
   // Register Grunt Task
-  grunt.registerMultiTask('static', 'Generate a static homepage', function() {
-
+  grunt.registerMultiTask('static', 'Generate a static homepage', function () {
     // Async Task
-    var done = this.async();
-    var options = this.options({});
-    var template = options.template;
+    var done = this.async()
+    var options = this.options({})
+    var template = options.template
 
-    grunt.verbose.writeflags(options, 'Options');
+    grunt.verbose.writeflags(options, 'Options')
 
     // Compile Jade templates and cache them.
-    var tplCache = Object.create(null);
-    var getJadeTpl = function(name) {
+    var tplCache = Object.create(null)
+    var getJadeTpl = function (name) {
       if (!tplCache[name]) {
-        var tplFileName = path.join(template, name + '.jade');
+        var tplFileName = path.join(template, name + '.jade')
 
-        tplCache[name] = fs.read(tplFileName).then(function(content) {
-          return jade.compile(content, {filename: tplFileName, cache: true, pretty: true});
-        });
+        tplCache[name] = fs.read(tplFileName).then(function (content) {
+          return jade.compile(content, {filename: tplFileName, cache: true, pretty: true})
+        })
       }
-      return tplCache[name];
-    };
+      return tplCache[name]
+    }
 
-    this.files.forEach(function(f) {
-
+    this.files.forEach(function (f) {
       // Options
-      var source = f.src[0];
-      var destination = f.dest[0];
+      var source = f.src[0]
+      var destination = f.dest[0]
 
-      grunt.log.writeln('Building files from \"' + source + '\" to \"' + destination + '\".');
+      grunt.log.writeln('Building files from "' + source + '" to "' + destination + '".')
 
       // Read all the markdown files
-      fs.listTree(source, filterOnlyFiles).then(function(files) {
-        return q.all(files.sort().map(function(filePath) {
-          return fs.read(filePath).then(function(content) {
-            var parts = filePath.substr(source.length).replace(/^\//, '').split('/');
-            var fileName = parts.pop();
-            var version = parts[0];
-            var section = parts[1] || null;
-            var basePath = parts.join('/') + '/';
-            var parsed = namp(content);
+      fs.listTree(source, filterOnlyFiles).then(function (files) {
+        return q.all(files.sort().map(function (filePath) {
+          return fs.read(filePath).then(function (content) {
+            var parts = filePath.substr(source.length).replace(/^\//, '').split('/')
+            var fileName = parts.pop()
+            var version = parts[0]
+            var section = parts[1] || null
+            var basePath = parts.join('/') + '/'
+            var parsed = namp(content)
 
             return {
               url: basePath + urlFromFilename(fileName),
@@ -97,43 +92,43 @@ module.exports = function(grunt) {
               showInMenu: parsed.metadata.showInMenu !== 'false',
               pageTitle: parsed.metadata.pageTitle || pageTitleFromFilename(fileName),
               editButton: parsed.metadata.editButton !== 'false'
-            };
-          });
-        }));
-      }).then(function(files) {
+            }
+          })
+        }))
+      }).then(function (files) {
         // construct the menu tree
-        var menu = Object.create(null);
-        files.forEach(function(file) {
-          if (!file.showInMenu) return;
+        var menu = Object.create(null)
+        files.forEach(function (file) {
+          if (!file.showInMenu) return
 
-          menu[file.version] = menu[file.version] || Object.create(null);
-          menu[file.version][file.section] = menu[file.version][file.section] || [];
-          menu[file.version][file.section].push(file);
-        });
+          menu[file.version] = menu[file.version] || Object.create(null)
+          menu[file.version][file.section] = menu[file.version][file.section] || []
+          menu[file.version][file.section].push(file)
+        })
 
         // generate and write all the html files
-        var versions = Object.keys(menu).sort(sortByVersion);
-        return q.all(files.map(function(file) {
-          var fileUrl = path.join(destination, file.url);
-          return q.all([getJadeTpl(file.layout), fs.makeTree(path.dirname(fileUrl))]).then(function(args) {
-            var jadeTpl = args[0];
+        var versions = Object.keys(menu).sort(sortByVersion)
+        return q.all(files.map(function (file) {
+          var fileUrl = path.join(destination, file.url)
+          return q.all([getJadeTpl(file.layout), fs.makeTree(path.dirname(fileUrl))]).then(function (args) {
+            var jadeTpl = args[0]
 
-            file.newUrl = file.url.replace(file.version, versions[0]);
+            file.newUrl = file.url.replace(file.version, versions[0])
             return fs.write(fileUrl, jadeTpl({
               versions: versions,
               oldVersion: file.version !== versions[0],
               editButton: file.editButton && file.version === versions[0],
               menu: menu[file.version],
               self: file
-            }));
-          });
-        }));
-      }).then(function() {
-        done();
-      }, function(e) {
-        grunt.fail.fatal(e.stack);
-        done(e);
-      });
-    });
-  });
-};
+            }))
+          })
+        }))
+      }).then(function () {
+        done()
+      }, function (e) {
+        grunt.fail.fatal(e.stack)
+        done(e)
+      })
+    })
+  })
+}
